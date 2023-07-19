e062b1e1dd209f00d4a098bc4d641770aacfcae2
Boris Kimel <borisk@rus9n.com>
2023-07-19 16:15:48 +0300

    Removed console.log calls

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 $("button#generate").on("click", async function() {
     let myAPIManager = new APIManager()
     await myAPIManager.fetchData()
-    console.log(myAPIManager.data)
     let myRenderer = new Renderer()
     myRenderer.render(myAPIManager.data)
 })
